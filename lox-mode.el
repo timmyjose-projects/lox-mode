c0b2b0748aeaca379d15c1bbfa801d02f7463be9
Timmy Jose <zoltan.jose@gmail.com>
2020-06-19 20:37:01 +0530

    Add run command option for Lox scripts

--- lox-mode.el
+++ lox-mode.el
@@ -1,4 +1,4 @@
-;;; lox-mode.el --- Major mode for the Lox programming language
+;;; lox-mode.el --- Major mode for the Lox programming language -*- lexical-binding: t -*-.
 
 ;; Copyright (C) 2020 Timmy Jose<zoltan.jose@gmail.com>
 
@@ -21,43 +21,54 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;  A major mode for the Lox programming language (http://craftinginterpreters.com/the-lox-language.html).
 
 ;;; Code:
 
+(defvar lox-mode-hook nil)
+
 (defgroup lox-mode nil
   "Lox mode configuration group."
   :link '(url-link "https://www.craftinginterpreters.com/the-lox-language.html")
   :group 'languages)
 
-(defcustom lox-bin "lox"
+(defcustom lox-bin "clox"
   "Path to the Lox executable."
   :type 'string
   :safe #'stringp
   :group 'lox-mode)
 
+;; supported commands
+
 ;;;###autoload
-(defun lox-compile-and-run-file ()
+(defun lox-run ()
   "Run the currently loaded script."
   (interactive)
-  (message "all done!"))
+  (compile (concat lox-bin " " (buffer-file-name)))
+  (other-window 1))
 
 (defvar lox-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-c") 'lox-compile-and-run-file)
+    (define-key map (kbd "C-c C-c") 'lox-run)
     map)
   "Keymap for Lox mode.")
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.lox\\'" . lox-mode))
 
+;; Derive the mode from java-mode for automatic syntax highlighting and
+;; indentation. Once a custom formatter is written for this mode, this
+;; can probably derive from prog-mode instead.
+
 ;;;###autoload
 (define-derived-mode lox-mode java-mode "Lox"
   "A major mode for the Lox programming language."
   :group 'lox-mode
+  (setq-local comment-start "// ")
+  (setq-local comment-end "")
   (use-local-map lox-mode-map))
 
 (provide 'lox-mode)
